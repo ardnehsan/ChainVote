8d27e3c18f84de10c59671a11839a2167be930e0
Your Name <hautran1993@yahoo.com>
2018-03-07 19:16:38 -0600

    old codes

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,10 @@
 // API Routes
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const LANDING = '/';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/pw-forget';
 var db = require("../models");
 
 module.exports = function (app) {
