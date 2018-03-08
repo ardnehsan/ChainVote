525c8e182ca74e58203fafd8c46e1bad6c630306
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-03-07 19:25:43 -0600

    updated blockchain routes, added mongoose structure

--- models/blockchain.js
+++ models/blockchain.js
@@ -1,31 +1,11 @@
-module.exports = function (sequelize, DataTypes) {
-    var BlockChain = sequelize.define("BlockChain", {
-        index: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        timestamp: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        data: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        previousHash: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        hash: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
 
+const blockChainSchema = new Schema ({
+  index: {type: Number, required: true},
+  timestamp: {type: Date, default: Date.now, required: true},
+  data: {type: String, require: true},
+  previousHash: {type: String, require: true},
+  hash: {type: String, require: true}
 
-        // // len is a validation that checks that our Voter is between 1 and 140 characters
-        //     validate: {
-        //     len: [1, 140]
-        // }
-    });
-    return BlockChain;
-};
+});
