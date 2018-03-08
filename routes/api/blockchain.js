66c590f00bdb62d0055f83973c7b05767db1a7d6
michaelnguyen55 <michaelnguyen55@yahoo.com>
2018-03-07 20:39:22 -0600

    Update

--- routes/api/blockchain.js
+++ routes/api/blockchain.js
@@ -3,6 +3,7 @@ const blockChainController = require ("../../controllers/blockChainController");
 
 // Matches with "/api/blockchain"
 router.route("/")
-.get(blockChainController.findAll);
+	.get(blockChainController.findAll)
+	.post(blockChainController.create);
 
 module.exports = router;
