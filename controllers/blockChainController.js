66ae4652f6b7dc146adc57bba08ce9022c122132
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-03-12 19:25:38 -0500

    updated file

--- controllers/blockChainController.js
+++ controllers/blockChainController.js
@@ -2,21 +2,12 @@ const db = require("../models");
 
 //Defining methods for BlockChain
 module.exports = {
-<<<<<<< HEAD
-  findAll: function(req, res){
-    db.BlockChain
-    .find(req.query)
-    .sort({date: -1})
-    .then(dbVoteChainBlockChain => res.json(dbVoteChainBlockChain))
-    .catch(err => res.status(422).json(err));
-=======
   findAll: function(req, res) {
     db.BlockChain
       .find(req.query)
       .sort({date: -1})
       .then(dbBlockChain => res.json(dbBlockChain))
       .catch(err => res.status(422).json(err));
->>>>>>> e5030e039f810fd948a7e701c8672564bfda8eb0
   },
   create: function(req, res) {
     const blockChain = {
@@ -30,8 +21,4 @@ module.exports = {
       .then(dbBlockChain => res.json(dbBlockChain))
       .catch(err => res.status(422).json(err));
   }
-<<<<<<< HEAD
-  
-=======
->>>>>>> e5030e039f810fd948a7e701c8672564bfda8eb0
 };
