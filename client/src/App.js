c6ca5fd383b8e622f42f75e129a5d0e7edbbc348
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-03-12 19:38:10 -0500

    removed multiple import pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,19 @@ import React from 'react';
 // BrowserRouter = IT TELLS REACT ROUTER HOW TO BEHAVE. IT LOOKS AT THE CURRENT URL AND CHANGE THE SET OF COMPONENTS VISIBLE ON THE SCREEN.
 // Route = IS A REACT COMPONENT THAT IS USED TO SETUP A RULE BETWEEN A ROUTE AND A SET OF COMPONENTS VISIBLE ON THE SCREEN.
 import { BrowserRouter, Route } from 'react-router-dom';
-<<<<<<< HEAD
-
 import Header from './components/Header';
 import LoginForm from './components/LoginForm';
 import Registration from './components/Registration';
 
-=======
-//inporting diffferent pages 
-import LoginForm from './components/LoginForm';
-import Registration from './components/Registration';
->>>>>>> e5030e039f810fd948a7e701c8672564bfda8eb0
+//inporting diffferent pages
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import Nav from "./components/Nav";
-
-
-const Landing = () => <h2>Landing</h2>
-=======
 import Landing from "./pages/Landing";
 import History from "./pages/History";
 // import Nav from "./components/Nav";
-import Header from './components/Header';
->>>>>>> e5030e039f810fd948a7e701c8672564bfda8eb0
+
+
 const Login = () => <h2>Login</h2>
 const CampaingNew = () => <h2>CampaingNew</h2>
 const Vote = () => <h2>Vote</h2>
@@ -52,12 +41,10 @@ const App = () => {
                     <Route exact path="/campaing" component={CampaingNew} />
                     <Route exact path="/vote" component={Vote} />
                     <Route exact path="/campaing/report" component={Report} />
-<<<<<<< HEAD
 
 
-=======
+
                     <Route exact path="/history" component={History} />
->>>>>>> e5030e039f810fd948a7e701c8672564bfda8eb0
                 </div>
             </BrowserRouter>
 
