756872a92eb1711f9be8dbddc79c7f6347d44031
Matthew-Lloyd <MLloyd09@gmail.com>
2018-03-12 20:55:17 -0500

    Building for production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -10,11 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build/index.html')));
 }
 
 // Add routes, both API and view
 app.use(routes);
+
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
