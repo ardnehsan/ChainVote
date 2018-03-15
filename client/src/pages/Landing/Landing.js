51497c437d996f6c676c3e0fcbd7f52b2f121ec7
Your Name <hautran1993@yahoo.com>
2018-03-15 17:42:28 -0500

    updating landing page basic show up for david to style

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,56 +1,37 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import {
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Jumbotron,
+  Label,
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 import API from "../../utils/API";
 
 class Landing extends Component {
-  state = {
-    articles: [],
-    q: "",
-    start_year: "",
-    end_year: "",
-    message: "Search For Articles To Begin!"
-  };
+        render() {
+        return(
+            <div classname="container">
+                <Button color="danger" onClick={this.props.handleFormSubmit}>Votes</Button>{' '}
+                <Button color="danger" onClick={this.props.handleFormSubmit}>History</Button>{' '}
+                
+                <h2>Vote Chain is a voting app that allow users to create campaigns and allow other users to vote.
+                This app is unique due to registration that has been create using our database. Only user who has been granted permission can enter the campaign to vote.</h2>
+            </div>
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  getArticles = () => {
-    API.getArticles({
-      q: this.state.q,
-      start_year: this.state.start_year,
-      end_year: this.state.end_year
-    })
-      .then(res =>
-        this.setState({
-          articles: res.data,
-          message: !res.data.length
-            ? "No New Articles Found, Try a Different Query"
-            : ""
-        })
-      )
-      .catch(err => console.log(err));
-  };
-
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     this.getArticles();
-//   };
-
-//   handleArticleSave = id => {
-//     const article = this.state.articles.find(article => article._id === id);
-//     API.saveArticle(article).then(res => this.getArticles());
-//   };
-
-  render() {
-    return (
-      <div>
-        <h1>Landing</h1>
-        </div>
-    );
-  }
-}
-
+        )
+    };
+};
 export default Landing;
