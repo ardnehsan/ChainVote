028e218858641e6eb22b599e2b9920e7a6d1f399
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-03-14 21:49:06 -0500

    upated sessions

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require ('path');
+const path = require('path');
 const app = express();
-const PeerServer = require ('peer').PeerServer;
-const cookieParser = require ('cookie-parser');
-const session = require ('express-sesison');
-const MongoStore = require ('connect-mongo')(session);
+//const PeerServer = require('peer').PeerServer;
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 // Serve up static assets
 app.use(express.static(path.join(__dirname, 'client/build/')));
 
-app.get('*', function (req, res) {
+app.get('*', function(req, res) {
   const index = path.join(__dirname, 'build', 'index.html');
   res.sendFile(index);
 });
@@ -30,7 +32,7 @@ mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/votechain");
-const db =mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, "Mongo DB connection error"));
 
 app.use(cookieParser());
@@ -38,11 +40,13 @@ app.use(session({
   secret: 'secret',
   saveUnitialized: true,
   resave: true,
-  store: new MongoStore({mongooseConnection: db})
+  store: new MongoStore({
+    mongooseConnection: db
+  })
 }))
 
 //Start persisent session for user
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   req.session.email;
   req.session.save();
 });
@@ -52,13 +56,15 @@ app.use(session({
   secret: 'secret',
   saveUnitialized: true,
   resave: true,
-  store: new MongoStore({mongooseConnection: db})
+  store: new MongoStore({
+    mongooseConnection: db
+  })
 }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/src/index.html');
-  if(req.session.email == undefined) {
+  if (req.session.email == undefined) {
     console.log("User not in sesison yet");
-  }else {
+  } else {
     console.log("Email from session: " + req.session.email);
   }
 });
