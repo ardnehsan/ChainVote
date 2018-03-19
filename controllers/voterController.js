b36b85d2532d2feab60c9c0f88b880cf586387bd
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-03-19 15:46:48 -0500

    matches master

--- controllers/voterController.js
+++ controllers/voterController.js
@@ -1,3 +1,4 @@
+
 const db = require("../models");
 const SHA256 = require('crypto-js/sha256');
 //Defining methods for Voter
@@ -5,35 +6,13 @@ module.exports = {
   login: function (req, res) {
     let email = req.query.email;
     let qpass = req.query.password;
-<<<<<<< HEAD
-    // let password = makeHash(req.body.password);
 
-// <<<<<<< HEAD
-// =======
-//     db.Voter
-//       .findOne({email : email})
-//       .then(dbVoter => {
-//           let voter = dbVoter;
-//           const vpass = SHA256(voter.password).toString();
-//
-//         if (vpass === qpass) {
-//           console.log('password good!')
-//           return res.json(true);
-//         };
-//         return res.json(false);
-//
-//         //bdea3eb189822ec26fb752c97e3c2b50fd87326af90d8ca01c5bf67d7b8d1a67
-//         })
-//       .catch(err => res.status(422).json(err));
-// >>>>>>> 0b80add7cff91b398bf9e6b175a86194a08fbeb9
-=======
- 
     db.Voter
       .findOne({email : email})
       .then(dbVoter => {
           let voter = dbVoter;
           const vpass = SHA256(voter.password).toString();
-         
+
         if (vpass === qpass) {
           // console.log('password good!')
           return res.json(dbVoter);
@@ -47,7 +26,6 @@ module.exports = {
     // console.log(req.query);
     let email = req.query.email;
     // // let lastName = req.query.lastName;
->>>>>>> b6ec159de3a3cc6ae235dabbaa7e36e5e1cc6b93
 
     db.Voter
       .findOne({
@@ -73,13 +51,8 @@ module.exports = {
         lastName : lastName
       })
       .then(dbVoter => {
-<<<<<<< HEAD
-        console.log(dbVoter);
-        if (dbVoter === null) {
-=======
         // console.log(dbVoter);
-        if (dbVoter === null) { 
->>>>>>> b6ec159de3a3cc6ae235dabbaa7e36e5e1cc6b93
+        if (dbVoter === null) {
           return false;
         } else {
           return res.json(dbVoter);
@@ -109,7 +82,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   //========================================================================================
-  
+
   Register: function(req, res) {
     const vfirstName = req.body.params.firstName;
     const vlastName = req.body.params.lastName;
@@ -130,7 +103,7 @@ module.exports = {
 
 //add users to Voter Table in votechain DB.
 
-// db.Voter.insert({"firstName": "Lysnandie", "lastName": "Jacques", "email": "LJacques@gmail.com", "password": "", "isRegistered": true})
+// db.Voter.insert({"firstName": "Lysandnie", "lastName": "Jacques", "email": "LJacques@gmail.com", "password": "", isRegistered": "false"})
 // db.Voter.insert({"firstName": "David", "lastName": "Morales", "email": "dmorales@gmail.com", "password": "", "isRegistered": true})
 // db.Voter.insert({"firstName": "Matthew", "lastName": "Lloyd", "email": "MLloyd@gmail.com", "password": "tester", "isRegistered": true})
 // db.Voter.insert({"firstName": "Michael", "lastName": "Nguyen", "email": "MNguyen@gmail.com", "password": "", "isRegistered": true})
--- server.js
+++ server.js
@@ -3,25 +3,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-<<<<<<< HEAD
-const path = require('path');
-=======
->>>>>>> b6ec159de3a3cc6ae235dabbaa7e36e5e1cc6b93
 const app = express();
-
-//const PeerServer = require('peer').PeerServer;
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-
 const PORT = process.env.PORT || 5000;
 
-// Add routes, both API and view
-app.use(routes);
 // Configure body parser for AJAX requests
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -41,55 +27,8 @@ app.get('*', function (req, res) {
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-
   process.env.MONGODB_URI || "mongodb://localhost/VoteChain");
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, "Mongo DB connection error"));
 
-
-app.use(cookieParser());
-app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true,
-  proxy: true,
-  store: new MongoStore({
-    mongooseConnection: db
-  })
-}))
-
-//Start persisent session for user
-app.use(function(req, res, next) {
-  req.session.firstName;
-  req.session.save();
-});
-
-//persisent login
-// app.use(session({
-//   secret: 'secret',
-//   saveUninitialized: true,
-//   resave: true,
-//   store: new MongoStore({
-//     mongooseConnection: db
-//   })
-// }))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/src/index.html');
-  if (req.session.firstName == undefined) {
-    console.log("User not in session yet");
-  } else {
-    console.log("Name from session: " + req.session.firstName);
-  }
-});
-
-//save user
-app.post('/firstName', function(req, res){
-  console.log("First name set to" + req.body.firstName);
-  req.session.firstName = req.body.firstName;
-  req.session.save();
-  console.log("Session value" + req.session.firstName);
-  res.end();
-});
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
